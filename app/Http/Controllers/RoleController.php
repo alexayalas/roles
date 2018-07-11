bd8a96048864779281a7d45c75a1c8a267db28e3
alexayalas <alejo.ayala.suncion@gmail.com>
2018-07-10 21:59:29 -0500

    primer commit

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Caffeinated\Shinobi\Models\Role;
+use Caffeinated\Shinobi\Models\Permission;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function __construct()
+    {        
+        $this->middleware('permission:roles.create')->only(['create','store']);        
+        $this->middleware('permission:roles.index')->only('index');                
+        $this->middleware('permission:roles.edit')->only(['edit','update']);        
+        $this->middleware('permission:roles.show')->only('show');        
+        $this->middleware('permission:roles.destroy')->only('destroy');               
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::paginate();
+
+        return view('roles.index', compact('roles'));
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Role $role)
+    {
+        return view('roles.show', compact('role'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Role $role)
+    {
+        $permissions = Permission::get();
+        return view('roles.edit', compact('role','permissions'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $permissions = Permission::get();
+
+        return view('roles.create',compact('permissions'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $role = Role::create($request->all());
+
+        $role->permissions()->sync($request->get('permissions'));        
+
+        return redirect()->route('roles.edit', $role->id)->with('info','Role guardado con exito');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $role->update($request->all());
+
+        $role->permissions()->sync($request->get('permissions'));
+        
+        return redirect()->route('roles.edit', $role->id)->with('info','rol actualizado con exito');
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+
+        return back()->with('info','Eliminado correctamente');
+    }
+}
